ea7aef4e13fb21ba83b8116eae4c823d70a0b284
TJCole03 <tjc6254@gmail.com>
2024-01-31 06:03:15 -0500

    client components/ notes taken in NewTodoForm

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 // import Image from 'next/image'
+import NewTodoForm from "@/components/NewTodoForm"
 
 const getData = async () => {
   await new Promise((res) => setTimeout(() => res(), 2000))
@@ -8,7 +9,12 @@ const getData = async () => {
 const  Home = async () => {
   const data = await getData()
   console.log(data)
-  return <div>Home - LCD Soundsystem</div>
+  return (
+    <>
+      <div>Home - LCD Soundsystem</div>
+      <NewTodoForm onChange={() => console.log('oawivns;')} />
+    </>
+  ) 
   
 }
 
---
+++ components/NewTodoForm.tsx
@@ -0,0 +1,43 @@
+//for typing stuff in and make new todo
+//client component
+'use client'
+import { useState } from "react"
+
+const NewTodoForm = ( {onChange} ) => {
+    const [state, updateState] = useState('')
+    
+    return (
+        <div>
+            <form>
+                <input type="text" />
+            </form>
+        </div>
+    )
+}
+
+export default NewTodoForm
+
+/*
+REMEMBER THIS:
+
+Server side rendering is when the component gets rendered on the server, 
+but then we'll have some Javascript that gets downloaded- this is the 
+exact replica of this componenet that then takes over
+
+Scott logged a window, which broke the server because there's no window on
+the server
+
+This is server side rendering; server components are close to the same
+thing except there is no Javascript on the front end that takes 
+over it. 
+
+The entire componenet is executed on the server so we can do database 
+activity.
+
+
+We can't pass non-serializable props between client and server components
+
+
+
+
+*/
