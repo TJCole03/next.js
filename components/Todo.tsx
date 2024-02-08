686786d83b18a3d449f916b146af248b7886c56a
TJCole03 <tjc6254@gmail.com>
2024-02-08 06:18:46 -0500

    server actions, able to cross out complete todos

--- components/Todo.tsx
+++ components/Todo.tsx
@@ -1,7 +1,22 @@
 //shows a todo
+'use client'
+import { useTransition } from "react"
+import { completeTodo } from "@/utils/actions"
+
 
 const Todo = ({ todo }) => {
-    return <div>{todo.content} </div>
+    const [isPending, startTransition] = useTransition()
+    // onClick is gonna take a callback, which calls startTransition, which takes a callback, 
+    // which calls completeTodo, and pass in the todo.id.
+    return (
+        <div
+            className={`border border-black/20 cursor-pointer
+                    ${todo.completed ?
+                    'line-through text-gray-600' : ''}`}
+            onClick={() => startTransition(() => completeTodo(todo.id))}>
+            {todo.content}
+        </div>
+    )
 }
 
 export default Todo
