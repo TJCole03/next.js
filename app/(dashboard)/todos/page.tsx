46ed7369b9b953ae41f097a0b51c5b0c79dc9870
TJCole03 <tjc6254@gmail.com>
2024-02-08 05:29:40 -0500

    loading and error files debugged

--- app/(dashboard)/todos/page.tsx
+++ app/(dashboard)/todos/page.tsx
@@ -3,6 +3,8 @@ import db from '@/utils/db'
 
 const getData = async () => {
     //"give me all the todos"
+    await new Promise ((resolve, reject) => setTimeout(() => resolve(), 1000))
+    // await new Promise ((resolve, reject) => setTimeout(() => reject(), 1000))
     const todos = await db.todo.findMany({})
     return todos
 }
