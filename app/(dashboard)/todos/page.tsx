0a1dbb0dd7442a2109777bdaca02ea907ff0089e
TJCole03 <tjc6254@gmail.com>
2024-02-14 05:30:27 -0500

    API routes q&a, a lot of notes

--- app/(dashboard)/todos/page.tsx
+++ app/(dashboard)/todos/page.tsx
@@ -3,7 +3,7 @@ import db from '@/utils/db'
 
 const getData = async () => {
     //"give me all the todos"
-    await new Promise ((resolve, reject) => setTimeout(() => resolve(), 1000))
+    // await new Promise ((resolve, reject) => setTimeout(() => resolve(), 1000))
     // await new Promise ((resolve, reject) => setTimeout(() => reject(), 1000))
     const todos = await db.todo.findMany({})
     return todos
--- app/api/todo/route.ts
+++ app/api/todo/route.ts
@@ -29,10 +29,67 @@ export const PUT = async (request: Request) => {
     const todo = await db.todo.update(data)
     return NextResponse.json({ message: todo})
 }
+export const DELETE = async (request: Request) => {
+    const data = await request.json()
+    const todo = await db.todo.delete({data,}) //not assignable to parameter of type...(see full message)
+    return NextResponse.json({ message: todo })
+}
 
 export const OPTIONS = () => {
-
+    return 
 }
+
+/*
+When would we use these APIs??
+    Not a whole lot. Here's why: 
+
+    The Next.js High Council wants us to use server-side fetching instead
+
+    We did that in the dashboard todos page:
+
+            { //...import data
+            
+            const getData = async () => {
+                //"give me all the todos"
+                // await new Promise ((resolve, reject) => setTimeout(() => resolve(), 1000))
+                // await new Promise ((resolve, reject) => setTimeout(() => reject(), 1000))
+                const todos = await db.todo.findMany({})
+                return todos
+            }
+
+            const TodosPage = async () => {
+                const todos = await getData()
+                return <div><TodoList todos={todos} /></div>
+            }
+                export default blahblahblah
+            
+            }
+
+They want you to do this, and they're right, you should be doing this. 
+This is way better.It's so much simpler. You don't have to go across the network. You're already on the server. 
+The sweet thing about doing this is now the person's Internet is not a factor in how fast they get the data back 
+because this happens on the server, which probably has fiber Internet. It's probably in some private network that's really quick. 
+And you're not relying on someone's network connection to determine how quickthey're gonna get the data back because it's not happening on the client.
+It's happening here, it's not going across the wire.
+
+
+*/
+
+/*
+      'Access-Control-Allow-Origin': '*', || 'anyone is allowed in, no matter which browser'
+      'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+      'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+
+Only browsers implement CORS. So if you have one server talking to another server, 
+CORS doesn't matter cuz only browsers look for CORS.
+
+*/
+
+/*
+differences between API routes in the pages directory versus API routes in the 
+app directory?
+
+*/
 
 /*
 if you want to get the data from the post request, typically,you do 
